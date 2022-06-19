97dcdeec6b3da1095451d8cc0e2be400a96f28c2
akidev9777 <akidev9777@gmail.com>
2022-06-19 14:52:56 +0900

    초기설정

---
+++ boot_oracle19c_jpa/src/test/java/com/example/boot_oracle19c_jpa/BootOracle19cJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boot_oracle19c_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootOracle19cJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
